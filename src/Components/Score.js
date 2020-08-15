f136f7dc069f64437ec7acca559de6ac86249215
Tino <tinomuzambi@gmail.com>
2020-08-16 00:26:52 +0200

    Added changing scores functionality.

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Score.css";
 
-const Score = () => (
-	<>
-		<div className="score">
-			<h1 className="curr-score">10</h1>
-			<div className="buttons">
-				<button className="button plus">+</button>
-				<button className="button minus">-</button>
-			</div>
-			<h1 className="player">Tino</h1>
-		</div>
-	</>
-);
+class Score extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			score: 0,
+		};
+
+		this.incScore = this.incScore.bind(this);
+		this.decScore = this.decScore.bind(this);
+	}
+
+	incScore() {
+		this.setState({ score: this.state.score + 1 });
+	}
+
+	decScore() {
+		this.state.score > 0
+			? this.setState({ score: this.state.score - 1 })
+			: this.setState({ score: 0 });
+	}
+
+	render() {
+		return (
+			<>
+				<div className="score">
+					<h1 className="curr-score">{this.state.score}</h1>
+					<div className="buttons">
+						<button className="button plus" onClick={this.incScore}>
+							+
+						</button>
+						<button className="button minus" onClick={this.decScore}>
+							-
+						</button>
+					</div>
+					<h1 className="player">Tino</h1>
+				</div>
+			</>
+		);
+	}
+}
 
 export default Score;
