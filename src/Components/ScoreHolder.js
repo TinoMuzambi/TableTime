6327055a57c62264d26d425f32b65dde4a000335
Tino <tinomuzambi@gmail.com>
2020-08-16 15:10:19 +0200

    Carries props in link.

--- src/Components/ScoreHolder.js
+++ src/Components/ScoreHolder.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ScoreHolder.css";
 import Score from "./Score";
 
-const ScoreHolder = () => (
-	<>
-		<div className="holder">
-			<h1 className="game-type">Game {21}</h1>
-			<Score gameType={21} player="Tino" />
-			<h1>-</h1>
-			<Score gameType={21} player="Talent" />
-		</div>
-	</>
-);
+class ScoreHolder extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			gameDetails: {
+				gameType: "",
+				bestOf: "",
+				player1: "",
+				player2: "",
+			},
+		};
+	}
+
+	componentDidMount() {
+		const { gameDetails } = this.props.location.state;
+		this.setState({ gameDetails: gameDetails });
+	}
+
+	render() {
+		return (
+			<>
+				<div className="holder">
+					<h1 className="game-type">Game {this.state.gameDetails.gameType}</h1>
+					<Score
+						gameType={this.state.gameDetails.gameType}
+						player={this.state.gameDetails.player1}
+					/>
+					<h1>-</h1>
+					<Score
+						gameType={this.state.gameDetails.gameType}
+						player={this.state.gameDetails.player2}
+					/>
+				</div>
+			</>
+		);
+	}
+}
 
 export default ScoreHolder;
--- src/Components/StartGame.js
+++ src/Components/StartGame.js
@@ -1,32 +1,116 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./StartGame.css";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowDown, MdArrowForward } from "react-icons/md";
 
-const StartGame = () => (
-	<>
-		<div className="holder">
-			<h1 className="heading">Table Time</h1>
-			<div className="player-input">
-				<h2 contentEditable="true">Tino</h2>
-			</div>
-			<h1>vs</h1>
-			<div className="player-input">
-				<h2 contentEditable="true">Tino</h2>
-			</div>
+class StartGame extends Component {
+	constructor() {
+		super();
 
-			<div class="dropdown">
-				<button class="dropbtn">
-					<h2>
-						Game 21 <MdKeyboardArrowDown />
-					</h2>
-				</button>
-				<div class="dropdown-content">
-					<a href="#">Game 21</a>
-					<a href="#">Game 11</a>
+		this.state = {
+			player1: "",
+			player2: "",
+			gameType: 11,
+			bestOf: "Best of 3",
+		};
+
+		this.setGameType = this.setGameType.bind(this);
+		this.setBestOf = this.setBestOf.bind(this);
+		this.setPlayer1 = this.setPlayer1.bind(this);
+		this.setPlayer2 = this.setPlayer2.bind(this);
+	}
+
+	setGameType(gameType) {
+		this.setState({ gameType: gameType });
+	}
+
+	setBestOf(bestOf) {
+		if (bestOf === 1) {
+			this.setState({ bestOf: "Single Game" });
+		} else if (bestOf === 3) {
+			this.setState({ bestOf: "Best of 3" });
+		} else {
+			this.setState({ bestOf: "Best of 5" });
+		}
+	}
+
+	setPlayer1(player1) {
+		this.setState({ player1: player1 });
+	}
+
+	setPlayer2(player2) {
+		this.setState({ player2: player2 });
+	}
+
+	render() {
+		return (
+			<>
+				<div className="holder">
+					<h1 className="heading">Table Time</h1>
+					<div className="container">
+						<div className="player-input">
+							<input
+								id="player1"
+								onChange={(e) => this.setPlayer1(e.target.value)}
+								placeholder="Player 1"
+							></input>
+						</div>
+						<h1>vs</h1>
+						<div className="player-input">
+							<input
+								id="player2"
+								onChange={(e) => this.setPlayer2(e.target.value)}
+								placeholder="Player 2"
+							></input>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<button class="dropbtn">
+							<h2>
+								Game {this.state.gameType} <MdKeyboardArrowDown />
+							</h2>
+						</button>
+						<div class="dropdown-content">
+							<span onClick={() => this.setGameType(21)}>Game 21</span>
+							<span onClick={() => this.setGameType(11)}>Game 11</span>
+						</div>
+					</div>
+
+					<div class="dropdown b">
+						<button class="dropbtn">
+							<h2>
+								{this.state.bestOf} <MdKeyboardArrowDown />
+							</h2>
+						</button>
+						<div class="dropdown-content">
+							<span onClick={() => this.setBestOf(1)}>Single Game</span>
+							<span onClick={() => this.setBestOf(3)}>Best of 3</span>
+							<span onClick={() => this.setBestOf(5)}>Best of 5</span>
+						</div>
+					</div>
+
+					<Link
+						to={{
+							pathname: "/game",
+							state: {
+								gameDetails: {
+									gameType: this.state.gameType,
+									bestOf: this.state.bestOf,
+									player1: this.state.player1,
+									player2: this.state.player2,
+								},
+							},
+						}}
+					>
+						<button className="button-start">
+							<MdArrowForward />
+						</button>
+					</Link>
 				</div>
-			</div>
-		</div>
-	</>
-);
+			</>
+		);
+	}
+}
 
 export default StartGame;
