84dc02b33b133df24f1e877a003959df6c9b15fb
Tinotenda Muzambi <tinomuzambi@gmail.com>
2020-08-19 06:27:58 +0200

    Adds backend code.

---
+++ src/build/precache-manifest.a6a532ff91aa71bac6de63598ef44677.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a5f34648223a8671e120fbbfa14504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2ada34f2ff3e77728dc",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "8710fde9e545b4830727",
+    "url": "/static/css/main.bafd4999.chunk.css"
+  },
+  {
+    "revision": "b2ada34f2ff3e77728dc",
+    "url": "/static/js/2.87b4b85a.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.87b4b85a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8710fde9e545b4830727",
+    "url": "/static/js/main.89500d31.chunk.js"
+  },
+  {
+    "revision": "4983f04cf3f0f241770c",
+    "url": "/static/js/runtime-main.03508d1f.js"
+  },
+  {
+    "revision": "d90b3bff9221bde91893bbc7a5083756",
+    "url": "/static/media/LexendDeca-Regular.d90b3bff.ttf"
+  }
+]);
