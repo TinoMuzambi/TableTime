e360407abc6de0fc1630981d19e8a2c40824b734
Tino <tinomuzambi@gmail.com>
2020-08-21 15:39:32 +0200

    Adds page transitions.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ import ScoreHolder from "./Components/ScoreHolder";
 import StartGame from "./Components/StartGame";
 import History from "./Components/History";
 import About from "./Components/About";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
 	return (
 		<>
 			<Router>
-				<Switch>
-					<Route path="/" component={SplashScreen} exact />
-					<Route path="/start" component={StartGame} />
-					<Route path="/game" component={ScoreHolder} />
-					<Route path="/history" component={History} />
-					<Route path="/about" component={About} />
-				</Switch>
+				<Route
+					render={({ location }) => (
+						<TransitionGroup>
+							<CSSTransition key={location.key} timeout={450} classNames="fade">
+								<Switch location={location}>
+									<Route path="/" component={SplashScreen} exact />
+									<Route path="/start" component={StartGame} />
+									<Route path="/game" component={ScoreHolder} />
+									<Route path="/history" component={History} />
+									<Route path="/about" component={About} />
+								</Switch>
+							</CSSTransition>
+						</TransitionGroup>
+					)}
+				/>
 			</Router>
 		</>
 	);
