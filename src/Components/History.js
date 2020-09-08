e94ca2505e257542269e7cc1eba5a0e644b9139c
Tino <tinomuzambi@gmail.com>
2020-09-08 03:28:30 +0200

    Moves secrets to process.env.

--- src/Components/History.js
+++ src/Components/History.js
@@ -53,7 +53,7 @@ class History extends Component {
 			await this.setState({ isFetching: false });
 		};
 
-		const pusher = new Pusher("e541d4f20f806b61b5d7", {
+		const pusher = new Pusher(process.env.pusher_key, {
 			cluster: "ap2",
 		});
 
