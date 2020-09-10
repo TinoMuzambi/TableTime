7e7b2ba544925a3f636e93a5d4dfeca39295f37f
Tino <tinomuzambi@gmail.com>
2020-09-10 22:57:43 +0200

    Adds toggling for dark mode.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import SplashScreen from "./Components/SplashScreen";
@@ -10,6 +10,33 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Helmet } from "react-helmet";
 
 function App() {
+	const [dark, setDark] = useState(false);
+
+	useEffect(() => {
+		const lastDark = localStorage.getItem("dark");
+		const btn = document.querySelector(".toggle");
+		if (lastDark === null) {
+			localStorage.setItem("dark", "false");
+		} else if (lastDark === "true") {
+			setDark(true);
+			document.documentElement.classList.add("dark-mode");
+			btn.innerHTML = "Light Mode";
+		} else {
+			setDark(false);
+			document.documentElement.classList.remove("dark-mode");
+			btn.innerHTML = "Dark Mode";
+		}
+	}, [dark]);
+
+	const toggleDark = () => {
+		const lastDark = localStorage.getItem("dark");
+		const btn = document.querySelector(".toggle");
+		document.documentElement.classList.toggle("dark-mode");
+		setDark(!dark);
+		localStorage.setItem("dark", `${lastDark === "false" ? "true" : "false"}`);
+		btn.innerHTML = lastDark === "false" ? "Dark Mode" : "Light Mode";
+	};
+
 	return (
 		<>
 			<Router>
@@ -18,70 +45,71 @@ function App() {
 						<TransitionGroup>
 							<CSSTransition key={location.key} timeout={450} classNames="fade">
 								<Switch location={location}>
-									<Route
-										path="/"
-										exact
-										render={() => (
-											<>
-												<Helmet>
-													<title>Table Time</title>
-												</Helmet>
-												<SplashScreen />
-											</>
-										)}
-									/>
-									<Route
-										path="/start"
-										exact
-										render={() => (
-											<>
-												<Helmet>
-													<title>Start | Table Time</title>
-												</Helmet>
-												<StartGame />
-											</>
-										)}
-									/>
-									<Route
-										path="/game"
-										exact
-										render={() => (
-											<>
-												<Helmet>
-													<title>Game | Table Time</title>
-												</Helmet>
-												<ScoreHolder />
-											</>
-										)}
-									/>
-									<Route
-										path="/history"
-										exact
-										render={() => (
-											<>
-												<Helmet>
-													<title>History | Table Time</title>
-												</Helmet>
-												<History />
-											</>
-										)}
-									/>
-									<Route
-										path="/about"
-										exact
-										render={() => (
-											<>
-												<Helmet>
-													<title>About | Table Time</title>
-												</Helmet>
-												<About />
-											</>
-										)}
-									/>
-									{/* <Route path="/start" component={StartGame} />
-									<Route path="/game" component={ScoreHolder} />
-									<Route path="/history" component={History} />
-									<Route path="/about" component={About} /> */}
+									<>
+										<button onClick={toggleDark} className="toggle">
+											Dark Mode
+										</button>
+										<Route
+											path="/"
+											exact
+											render={() => (
+												<>
+													<Helmet>
+														<title>Table Time</title>
+													</Helmet>
+													<SplashScreen />
+												</>
+											)}
+										/>
+										<Route
+											path="/start"
+											exact
+											render={() => (
+												<>
+													<Helmet>
+														<title>Start | Table Time</title>
+													</Helmet>
+													<StartGame />
+												</>
+											)}
+										/>
+										<Route
+											path="/game"
+											exact
+											render={() => (
+												<>
+													<Helmet>
+														<title>Game | Table Time</title>
+													</Helmet>
+													<ScoreHolder />
+												</>
+											)}
+										/>
+										<Route
+											path="/history"
+											exact
+											render={() => (
+												<>
+													<Helmet>
+														<title>History | Table Time</title>
+													</Helmet>
+													<History />
+												</>
+											)}
+										/>
+										<Route
+											path="/about"
+											exact
+											render={() => (
+												<>
+													<Helmet>
+														<title>About | Table Time</title>
+													</Helmet>
+													<About />
+												</>
+											)}
+										/>
+									</>
 								</Switch>
 							</CSSTransition>
 						</TransitionGroup>
