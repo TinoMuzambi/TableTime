02621c7111420ebb75d098c56ccd9196d3c87544
Tino <tinomuzambi@gmail.com>
2020-09-15 03:22:04 +0200

    Start to flex.

--- src/pages/StartGame.js
+++ src/pages/StartGame.js
@@ -44,53 +44,61 @@ class StartGame extends Component {
 		return (
 			<>
 				<div className="start-holder">
-					<button
-						className="start-back-button"
-						onClick={this.props.history.goBack}
-					>
-						<MdArrowBack className="button-link" />
-					</button>
-					<h1 className="heading">Table Time</h1>
-					<div className="player-input player1">
-						<input
-							id="player1"
-							onChange={(e) => this.setPlayer1(e.target.value)}
-							placeholder="Player 1"
-						></input>
-					</div>
-					<h1 className="vs">vs</h1>
-					<div className="player-input player2">
-						<input
-							id="player2"
-							onChange={(e) => this.setPlayer2(e.target.value)}
-							placeholder="Player 2"
-						></input>
+					<div className="flex">
+						<button
+							className="start-back-button"
+							onClick={this.props.history.goBack}
+						>
+							<MdArrowBack className="button-link" />
+						</button>
+						<h1 className="heading">Table Time</h1>
 					</div>
 
-					<div className="game">
-						<select
-							name="game"
-							id="game"
-							onChange={(e) => this.setGameType(e.target.value)}
-						>
-							<option value="11">Game 11</option>
-							<option value="21">Game 21</option>
-						</select>
-						<MdKeyboardArrowDown className="game-arrow" />
+					<div className="flex2">
+						<div className="player-input player1">
+							<input
+								id="player1"
+								onChange={(e) => this.setPlayer1(e.target.value)}
+								placeholder="Player 1"
+							></input>
+						</div>
+						<h1 className="vs">vs</h1>
+						<div className="player-input player2">
+							<input
+								id="player2"
+								onChange={(e) => this.setPlayer2(e.target.value)}
+								placeholder="Player 2"
+							></input>
+						</div>
 					</div>
 
-					<div className="rounds">
-						<select
-							name="rounds"
-							id="rounds"
-							onChange={(e) => this.setBestOf(e.target.value)}
-						>
-							<option value="Single Game">Single Game</option>
-							<option value="Best of 3">Best of 3</option>
-							<option value="Best of 5">Best of 5</option>
-						</select>
-						<MdKeyboardArrowDown className="rounds-arrow" />
+					<div className="flex3">
+						<div className="game">
+							<select
+								name="game"
+								id="game"
+								onChange={(e) => this.setGameType(e.target.value)}
+							>
+								<option value="11">Game 11</option>
+								<option value="21">Game 21</option>
+							</select>
+							<MdKeyboardArrowDown className="game-arrow" />
+						</div>
+
+						<div className="rounds">
+							<select
+								name="rounds"
+								id="rounds"
+								onChange={(e) => this.setBestOf(e.target.value)}
+							>
+								<option value="Single Game">Single Game</option>
+								<option value="Best of 3">Best of 3</option>
+								<option value="Best of 5">Best of 5</option>
+							</select>
+							<MdKeyboardArrowDown className="rounds-arrow" />
+						</div>
 					</div>
+
 					<button className="button-start">
 						{/* Link to state and pass game details to ScoreHolder component. */}
 						<Link
