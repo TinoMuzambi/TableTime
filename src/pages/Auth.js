50d88d23138d8bcad9af53458a8d93da08423561
Tino <tinomuzambi@gmail.com>
2020-09-15 17:48:46 +0200

    Notice message fix.

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { MdArrowBack } from "react-icons/md";
-import { withRouter } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
 
 class Auth extends Component {
 	constructor() {
 		super();
 
-		this.state = { curr: "Login", username: "", password: "" };
+		this.state = {
+			curr: "Login",
+			username: "",
+			password: "",
+			loggedIn: false,
+			redirect: false,
+		};
 
 		this.auth = this.auth.bind(this);
 	}
@@ -28,65 +34,41 @@ class Auth extends Component {
 		}).then((response) => {
 			const success = document.querySelector(".status-block-success");
 			const failure = document.querySelector(".status-block-failure");
+			const notice = document.querySelector(".notice");
+			notice.classList.add("shown");
 			response.text().then((res) => {
 				failure.firstChild.innerText = res;
 			});
-			response.status === 200 || response.status === 201
-				? success.classList.add("shown")
-				: failure.classList.add("shown");
+			if (response.status === 200) {
+				success.classList.add("shown");
+				this.setState({ redirect: true, loggedIn: true });
+				if (this.state.redirect) {
+					return <Redirect to="/" />;
+				}
+			} else if (response.status === 201) {
+				success.classList.add("shown");
+			} else {
+				console.log("failure");
+				failure.classList.add("shown");
+			}
 			response.status === 200 || response.status === 201
 				? setTimeout(() => {
 						success.classList.remove("shown");
+						notice.classList.remove("shown");
 						this.setState({ username: "" });
 						this.setState({ password: "" });
 				  }, 5000)
 				: setTimeout(() => {
 						failure.classList.remove("shown");
+						notice.classList.remove("shown");
 				  }, 5000);
 		});
 	}
 
 	render() {
 		return (
-			<div className="auth-holder">
-				<div className="flex">
-					<button
-						className="auth-back-button"
-						onClick={this.props.history.goBack}
-					>
-						<MdArrowBack className="button-link" />
-					</button>
-					<h1 className="title">{this.state.curr}</h1>
-					<button
-						className="toggler"
-						onClick={() => {
-							this.setState({
-								curr: this.state.curr === "Login" ? "Register" : "Login",
-							});
-						}}
-					>
-						Register
-					</button>
-				</div>
-
-				<div className="col">
-					<form onSubmit={this.auth} className="form">
-						<input
-							type="text"
-							placeholder="username"
-							className="input user"
-							value={this.state.username}
-							onChange={(e) => this.setState({ username: e.target.value })}
-						/>
-						<input
-							type="password"
-							placeholder="password"
-							className="input password"
-							value={this.state.password}
-							onChange={(e) => this.setState({ password: e.target.value })}
-						/>
-						<input type="submit" value={this.state.curr} className="submit" />
-					</form>
+			<>
+				<div className="notice">
 					<div className="status-block-success">
 						<p className="text">Success</p>
 					</div>
@@ -94,7 +76,48 @@ class Auth extends Component {
 						<p className="text">Logged In</p>
 					</div>
 				</div>
-			</div>
+				<div className="auth-holder">
+					<div className="flex">
+						<button
+							className="auth-back-button"
+							onClick={this.props.history.goBack}
+						>
+							<MdArrowBack className="button-link" />
+						</button>
+						<h1 className="title">{this.state.curr}</h1>
+						<button
+							className="toggler"
+							onClick={() => {
+								this.setState({
+									curr: this.state.curr === "Login" ? "Register" : "Login",
+								});
+							}}
+						>
+							Register
+						</button>
+					</div>
+
+					<div className="col">
+						<form onSubmit={this.auth} className="form">
+							<input
+								type="text"
+								placeholder="username"
+								className="input user"
+								value={this.state.username}
+								onChange={(e) => this.setState({ username: e.target.value })}
+							/>
+							<input
+								type="password"
+								placeholder="password"
+								className="input password"
+								value={this.state.password}
+								onChange={(e) => this.setState({ password: e.target.value })}
+							/>
+							<input type="submit" value={this.state.curr} className="submit" />
+						</form>
+					</div>
+				</div>
+			</>
 		);
 	}
 }
